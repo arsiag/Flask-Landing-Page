85cfa348d6be2022ef0c7d2a263f02ffd41e06d3
Arsia Ghahari <arsia.ghahari@gmail.com>
2017-09-07 17:22:20 -0700

    frist commit

---
+++ LandingPage.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/ninjas')
+def ninjas():
+    return render_template('ninjas.html')  
+
+@app.route('/dojos/new')
+def create_dojo():
+   print "Dojo Info"
+   return render_template("dojos.html")
+
+app.run(debug=True) # run our server
